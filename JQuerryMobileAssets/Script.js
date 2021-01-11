65d95e1d61aec4a6cdf43f9781acf250e124b1a0
sandaru123 <33059684+sandaru123@users.noreply.github.com>
2021-01-12 04:32:50 +0530

    payment

--- JQuerryMobileAssets/Script.js
+++ JQuerryMobileAssets/Script.js
@@ -1,3 +1,11 @@
+$(".cash_payment").click(function(){
+    $(".paymentDetails").hide();
+    $(".paymentDetailsH").hide();
+  });
+  
+  $(".debitCredit_payment").click(function(){
+    $(".paymentDetails").show();
+    $(".paymentDetailsH").show();
+  });
 
-
-
+  
