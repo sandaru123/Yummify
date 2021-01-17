71f9b4ae7fd2ec47bc710736154b1c69680025ae
Pasan Dil <pasandileepadissanayake@gmail.com>
2021-01-18 03:02:55 +0530

    added add to fav list

--- Scripts/Faviorite.js
+++ Scripts/Faviorite.js
@@ -1,196 +1,188 @@
-var cardslist = [
-	{ itemName: 'Chorizo & Mozzarella', itemDetail: 'Butter chicken curry or murg makhani, butter and cream sauce.', price: '55$', imagesource: '../Images/burgers.jpg', rating: 4, favornot: 'fa fa-heart fa-lg',faviorite : 1 },
-	{ itemName: 'Italian panettone', itemDetail: 'Bake in the preheated oven for 15 minutes', price: '15$', imagesource: '../Images/desserts.jpg', rating: 2, favornot: 'fa fa-heart fa-lg',faviorite : 0 },
-	{ itemName: 'Mayo deviled eggs', itemDetail: 'stirring halfway through the baking.', price: '11$', imagesource: '../Images/drinks.jpg', rating: 3, favornot: 'fa fa-heart fa-lg',faviorite : 1 },
-	{ itemName: 'Rosemary Baked Olives', itemDetail: 'Butter chicken curry or murg makhani is a curry', price: '5$', imagesource: '../Images/indian.jpg', rating: 4, favornot: 'fa fa-heart-o fa-lg',faviorite :0 },
-  { itemName: 'boeuf bourguignon', itemDetail: 'Beef brisket, red wine, small pearl onions, bacon, tomato paste', price: '150$', imagesource: '../Images/Boeuf-Bourguignon.jpg', rating: 5, favornot: 'fa fa-heart fa-lg',faviorite :1 },
-  { itemName: 'KFC Original Recipe Chicken', itemDetail: 'Secret 11 herbs and spices and for launching ', price: '500$', imagesource: '../Images/kfc-original-bucket-fried-chicken.jpg', rating: 1, favornot: 'fa fa-heart fa-lg',faviorite :1 },
-  { itemName: 'Dunkin Coffee', itemDetail: 'Forget the donuts! Rebranded as just Dunkin in 2018', price: '100$', imagesource: '../Images/dunkin-coffee-cup.jpg', rating: 3, favornot: 'fa fa-heart fa-lg',faviorite :1 },
-  { itemName: 'Kung Pao Chicken', itemDetail: 'This spicy stir-fry dish is a Panda Express classic', price: '100$', imagesource: '../Images/panda-express-kung-pao-chicken.jpg', rating: 2, favornot: 'fa fa-heart fa-lg',faviorite :1 }
-];
-
 var cardItemslist = JSON.parse(localStorage.getItem("cardsDetails_Local"));
+
+console.log(cardItemslist);
 
 // function to create the cards
 function createFaviriteCards(sortValue) {
-	// declaring some variables
-	var exampleBlockA = $('#exampleBlockA'), // cache the selector of the element, increases performance
-		exampleBlockB = $('#exampleBlockB'), // cache the selector of the element, increases performance
-		exampleBlockC = $('#exampleBlockC'), // cache the selector of the element, increases performance
-		i,
-		j = 1,
+  // declaring some variables
+  var exampleBlockA = $('#exampleBlockA'), // cache the selector of the element, increases performance
+    exampleBlockB = $('#exampleBlockB'), // cache the selector of the element, increases performance
+    exampleBlockC = $('#exampleBlockC'), // cache the selector of the element, increases performance
+    i,
+    j = 1,
 
-		// the cards, in this example in an array
     // the cards, in this example in an array
-		imagename = ["Image1", "Image2", "Image3", "Image4"],
-		imagesource = ["Images/burgers.jpg", "./images/image2.jpg", "./images/image3.jpg", ".Images/promo\ 1.jpg"],
-		itemName = ["Chorizo & Mozzarella", "Italian panettone", "Mayo deviled eggs", "Rosemary Baked Olives"],
-		itemDetail = ["Butter chicken curry or murg makhani, butter and cream sauce.", "Butter chicken curry or murg makhani is a curry, butter.", "Bake in the preheated oven for 15 minutes", "stirring halfway through the baking."],
-		titlecard = ["Image!", "Image!", "Image!", "Image!"],
-		titleimage = ["Title", "Title", "Title", "Title"],
-		text = ["Lorem ipsum part 1...", "Lorem ipsum part 2...", "Lorem ipsum part 3...", "Lorem ipsum part 3..."];
-		
+    // the cards, in this example in an array
+    imagename = ["Image1", "Image2", "Image3", "Image4"],
+    imagesource = ["Images/burgers.jpg", "./images/image2.jpg", "./images/image3.jpg", ".Images/promo\ 1.jpg"],
+    itemName = ["Chorizo & Mozzarella", "Italian panettone", "Mayo deviled eggs", "Rosemary Baked Olives"],
+    itemDetail = ["Butter chicken curry or murg makhani, butter and cream sauce.", "Butter chicken curry or murg makhani is a curry, butter.", "Bake in the preheated oven for 15 minutes", "stirring halfway through the baking."],
+    titlecard = ["Image!", "Image!", "Image!", "Image!"],
+    titleimage = ["Title", "Title", "Title", "Title"],
+    text = ["Lorem ipsum part 1...", "Lorem ipsum part 2...", "Lorem ipsum part 3...", "Lorem ipsum part 3..."];
 
-	// emptying the current grid
-	exampleBlockA.empty();
-	exampleBlockB.empty();
-	exampleBlockC.empty();
 
-	var tag = '';
+  // emptying the current grid
+  exampleBlockA.empty();
+  exampleBlockB.empty();
+  exampleBlockC.empty();
 
-	for (i = 0; i < cardItemslist.length; i++) {
-        if(cardItemslist[i].faviorite == 1){
-          tag = '';
-          tag += '<div class=" card maincardwidth favoriteCardsDiv" style="height: 200px;">';
-          tag += '<div class="flexcolumn">';
-          tag += '<div id="card' + (i + 1) + '" class="flexrow" style="height: 160px;">';
-          tag += '<div style="width: 180px; height:160px;">';
-          tag += '<img class="card-image cardimageedit" style="height: 140px;" alt="image" src="' + cardItemslist[i].imagesource + '" />';
-          tag += '</div>';
-          tag += '<div class="flexcolumn" style="width: 200px;">';
-          tag += '<div class="flexrow" style="height: auto;">';
-          tag += '<div>';
-          tag += '<p style="font-size: 15px; font-weight: bold;">' + cardItemslist[i].itemName + '</p>';
-          tag += '</div>';
-          tag += '<div>';
-          tag += '<p style="font-size: 15px; font-weight: bold; color: #33D9B2;">' + cardItemslist[i].price + '</p>';
-          tag += '</div>';
-          tag += '</div>';
-          tag += '<div style="height: auto;" class="justify-content-end">';
-          tag += '<p style="font-size: 13px; margin-top: -10px; color:black;">by Janice Cheddar</p>';
-          tag += '<p style="font-size: 12px; margin-top: -18px; color:#6c6f71">' + cardItemslist[i].itemDetail + '</p>';
-          tag += '</div>';
-          tag += '</div>';
-          tag += '</div>';
-          tag += '<div class="flexcolumn" style="height: 20px;">';
-          tag += '<div>';
-          tag += '<span class="fa fa-star starcoluryello"></span>';
-          tag += '<span class="">' + cardItemslist[i].rating + '</span>';
-          tag += '</div>';
-          tag += '<div style="display:flex; position: absolute; margin-right: 10px; right: 0; top: 160px;"><a class="favioriteI" id=" ' + i + ' "><i style="color: red;"  class=" ' + cardItemslist[i].favornot + ' mr-1 p-1"></i> </a> &nbsp <a class="cartFavorite"><i class="fa fa-shopping-cart fa-lg mr-1 p-1"></i></a>  </div>';
-          tag += '</div>';
-          tag += '</div>';
-          tag += '</div>';
+  var tag = '';
+
+  for (i = 0; i < cardItemslist.length; i++) {
+
+    if (cardItemslist[i].faviorite == 1) {
     
-            /*	You will need to create cards in a special order.
-                The first 1/3 of the cards are placed in block A.
-                The second 1/3 of the cards are placed in block B.
-                The last 1/3 of the cards are placed in block C.
-                
-                This will make sure that the cards will fill white spots
-                when the screen is changing orientation and/or size.
-                
-                When you create new block for every card you would get
-                an interface that is lined like a table.
-            */
-    
-            if (i < (imagename.length / 3)) {
-                exampleBlockA.append(tag);
-            } else if (i < ((imagename.length / 3) * 2)) {
-                exampleBlockB.append(tag);
-            } else if (i <= ((imagename.length / 3) * 3)) {
-                exampleBlockC.append(tag);
-            }
-    
-            // add a press effect to the card
-            pressEffectCard('card' + i);
-            }
+      tag = '';
+      tag += '<div class=" card maincardwidth favoriteCardsDiv" style="height: 200px;">';
+      tag += '<div class="flexcolumn">';
+      tag += '<div id="card' + (i + 1) + '" class="flexrow" style="height: 160px;">';
+      tag += '<div style="width: 180px; height:160px;">';
+      tag += '<img class="card-image cardimageedit" style="height: 140px;" alt="image" src="' + cardItemslist[i].imagesource + '" />';
+      tag += '</div>';
+      tag += '<div class="flexcolumn" style="width: 200px;">';
+      tag += '<div class="flexrow" style="height: auto;">';
+      tag += '<div>';
+      tag += '<p style="font-size: 15px; font-weight: bold;">' + cardItemslist[i].itemName + '</p>';
+      tag += '</div>';
+      tag += '<div>';
+      tag += '<p style="font-size: 15px; font-weight: bold; color: #33D9B2;">' + cardItemslist[i].price + '</p>';
+      tag += '</div>';
+      tag += '</div>';
+      tag += '<div style="height: auto;" class="justify-content-end">';
+      tag += '<p style="font-size: 13px; margin-top: -10px; color:black;">by Janice Cheddar</p>';
+      tag += '<p style="font-size: 12px; margin-top: -18px; color:#6c6f71">' + cardItemslist[i].itemDetail + '</p>';
+      tag += '</div>';
+      tag += '</div>';
+      tag += '</div>';
+      tag += '<div class="flexcolumn" style="height: 20px;">';
+      tag += '<div>';
+      tag += '<span class="fa fa-star starcoluryello"></span>';
+      tag += '<span class="">' + cardItemslist[i].rating + '</span>';
+      tag += '</div>';
+      tag += '<div style="display:flex; position: absolute; margin-right: 10px; right: 0; top: 160px;"><a class="favioriteI" id=" ' + i + ' "><i style="color: red;"  class=" ' + cardItemslist[i].favornot + ' mr-1 p-1"></i> </a> &nbsp <a class="cartFavorite"><i class="fa fa-shopping-cart fa-lg mr-1 p-1"></i></a>  </div>';
+      tag += '</div>';
+      tag += '</div>';
+      tag += '</div>';
 
-		
-	}
+      /*	You will need to create cards in a special order.
+          The first 1/3 of the cards are placed in block A.
+          The second 1/3 of the cards are placed in block B.
+          The last 1/3 of the cards are placed in block C.
+          
+          This will make sure that the cards will fill white spots
+          when the screen is changing orientation and/or size.
+          
+          When you create new block for every card you would get
+          an interface that is lined like a table.
+      */
+
+      if (i < (imagename.length / 3)) {
+        exampleBlockA.append(tag);
+      } else if (i < ((imagename.length / 3) * 2)) {
+        exampleBlockB.append(tag);
+      } else if (i <= ((imagename.length / 3) * 3)) {
+        exampleBlockC.append(tag);
+      }
+
+      // add a press effect to the card
+      pressEffectCard('card' + i);
+    }
+
+  }
 }
 
 // press effect card ui
 function pressEffectCard(x) {
-	var id = $("#" + x); // cache the selector of the element, increases performance
-	id.off('touchstart').on('touchstart', function () {
-		id.addClass("holoPressEffectDiv");
-	});
-	id.off('touchend').on('touchend', function () {
-		id.removeClass("holoPressEffectDiv");
-	});
-	id.off('touchmove').on('touchmove', function () {
-		id.removeClass("holoPressEffectDiv"); // to remove the press effect when there is a scroll detected in stead of a tap
-	});
+  var id = $("#" + x); // cache the selector of the element, increases performance
+  id.off('touchstart').on('touchstart', function () {
+    id.addClass("holoPressEffectDiv");
+  });
+  id.off('touchend').on('touchend', function () {
+    id.removeClass("holoPressEffectDiv");
+  });
+  id.off('touchmove').on('touchmove', function () {
+    id.removeClass("holoPressEffectDiv"); // to remove the press effect when there is a scroll detected in stead of a tap
+  });
 }
 
 
-$(".cash_payment").click(function(){
-    $(".paymentDetails").hide();
-    $(".paymentDetailsH").hide();
-  });
-  
-  $(".debitCredit_payment").click(function(){
-    $(".paymentDetails").show();
-    $(".paymentDetailsH").show();
-  });
-
-  $("#placeOrder").click(function(){
-    var points = $("#use_points").html();
-    var payment_total = $("#payment_total").html();
-
-    var reduced_total = parseInt(payment_total,10) - parseInt(points,10);
-    $('#subtotal_payment_summary').html(reduced_total.toString());
-	$("#summaryLink").click();
-  });
-  
-
-  $(document).on("click", ".cartFavorite" , function() {
-    alert("Item Added to the Cart");
-  });
-
-
-  $(document).on("click", ".favioriteI" , function() {
-    var id= $(this).attr('id');
-    var idInt = parseInt(id,10);
-    //Find index of specific object using findIndex method.    
-    objIndex = cardItemslist.findIndex((obj => obj.id == 0));
-
-    //Log object to Console.
-    console.log("Before update: ", cardItemslist[objIndex])
-
-    //Update object's name property.
-    cardItemslist[idInt].faviorite = 0;
-
-    localStorage.setItem("cardsDetails_Local", JSON.stringify(cardItemslist));
-    var cardItemslist121 = JSON.parse(localStorage.getItem("cardsDetails_Local"));
-
-    console.log(cardItemslist121)
-
-    $("div .favoriteCardsDiv").remove();
-    createFaviriteCards("Nme");
+$(".cash_payment").click(function () {
+  $(".paymentDetails").hide();
+  $(".paymentDetailsH").hide();
 });
 
-  // $(".favioriteI").on('click', function () {
-  //   var id= this.attr('id');
-  //     var idInt = parseInt(id,10);
-  //     //Find index of specific object using findIndex method.    
-  //     objIndex = cardItemslist.findIndex((obj => obj.id == idInt));
+$(".debitCredit_payment").click(function () {
+  $(".paymentDetails").show();
+  $(".paymentDetailsH").show();
+});
 
-  //     //Log object to Console.
-  //     console.log("Before update: ", cardItemslist[objIndex])
+$("#placeOrder").click(function () {
+  var points = $("#use_points").html();
+  var payment_total = $("#payment_total").html();
 
-  //     //Update object's name property.
-  //     cardItemslist[objIndex].faviorite = 0;
+  var reduced_total = parseInt(payment_total, 10) - parseInt(points, 10);
+  $('#subtotal_payment_summary').html(reduced_total.toString());
+  $("#summaryLink").click();
+});
 
-  //     $("div .favoriteCardsDiv").remove();
-	// 		createFaviriteCards("Nme");
-  // })
 
-  
+$(document).on("click", ".cartFavorite", function () {
+  alert("Item Added to the Cart");
+});
 
-  $(document).ready(function(){
-	$("#selectDropdown").change(function(){
-	  //selection changedvar 
-	  selText =  $( "#selectDropdown option:selected" ).text();
-	  
-	  if(selText == "Sort By Name"){
-		$("div .maincardwidth").remove();
-		createCards("Name");
 
-	  }else if(selText == "Sort By Price"){
-		$("div .maincardwidth").remove();
-		createCards("Price");
-	  }  
+$(document).on("click", ".favioriteI", function () {
+  var id = $(this).attr('id');
+  var idInt = parseInt(id, 10);
+  //Find index of specific object using findIndex method.    
+  objIndex = cardItemslist.findIndex((obj => obj.id == 0));
+
+  //Log object to Console.
+  console.log("Before update: ", cardItemslist[objIndex])
+
+  //Update object's name property.
+  cardItemslist[idInt].faviorite = 0;
+
+  localStorage.setItem("cardsDetails_Local", JSON.stringify(cardItemslist));
+  var cardItemslist121 = JSON.parse(localStorage.getItem("cardsDetails_Local"));
+
+  console.log(cardItemslist121)
+
+  $("div .favoriteCardsDiv").remove();
+  createFaviriteCards("Nme");
+});
+
+// $(".favioriteI").on('click', function () {
+//   var id= this.attr('id');
+//     var idInt = parseInt(id,10);
+//     //Find index of specific object using findIndex method.    
+//     objIndex = cardItemslist.findIndex((obj => obj.id == idInt));
+
+//     //Log object to Console.
+//     console.log("Before update: ", cardItemslist[objIndex])
+
+//     //Update object's name property.
+//     cardItemslist[objIndex].faviorite = 0;
+
+//     $("div .favoriteCardsDiv").remove();
+// 		createFaviriteCards("Nme");
+// })
+
+
+
+$(document).ready(function () {
+  $("#selectDropdown").change(function () {
+    //selection changedvar 
+    selText = $("#selectDropdown option:selected").text();
+
+    if (selText == "Sort By Name") {
+      $("div .maincardwidth").remove();
+      createCards("Name");
+
+    } else if (selText == "Sort By Price") {
+      $("div .maincardwidth").remove();
+      createCards("Price");
+    }
   });
-  });
+});
--- Scripts/addtofavlist.js
+++ Scripts/addtofavlist.js
@@ -1,15 +1,19 @@
 function inifavlocalhost(addfavitemid) {
 
-    // Retrieve the object from storage
-    var retrievedObject = JSON.parse(localStorage.getItem('favitems'));
+    var id= addfavitemid;
+    var idInt = parseInt(id,10);
+    //Find index of specific object using findIndex method.    
+    objIndex = cardItemslist.findIndex((obj => obj.id == 0));
 
-    if (!retrievedObject.includes(addfavitemid)) {
-        retrievedObject.push(addfavitemid);
-        localStorage.setItem('favitems', JSON.stringify(retrievedObject));
-        console.log(JSON.parse(localStorage.getItem('favitems')));
-        alert("Added to Fav List!!");
-    } else {
-        alert("Already in Fav List!!");
-    }
+    //Log object to Console.
+    console.log("Before update: ", cardItemslist[objIndex])
+
+    //Update object's name property.
+    cardItemslist[idInt].faviorite = 1;
+
+    localStorage.setItem("cardsDetails_Local", JSON.stringify(cardItemslist));
+    var cardItemslist121 = JSON.parse(localStorage.getItem("cardsDetails_Local"));
+
+    console.log(cardItemslist121)
 
 }
