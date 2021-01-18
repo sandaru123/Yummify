806df7f9660fd784dab74ecf16dfcb1c6e2082f6
Kumuditha Perera <kumudithaperera@gmail.com>
2021-01-18 05:33:24 +0530

    products page modal and buttons styling done

--- Scripts/ProductDetails.js
+++ Scripts/ProductDetails.js
@@ -1,26 +1,35 @@
-
+var modal = document.getElementById("RatingModalDetails");
+var btn = document.getElementById("ratingPopupButton");
+var span = document.getElementsByClassName("close")[0];
 
 $(document).ready(function () {
 
-    // Attach Button click event listener 
-   $("#ratingPopupButton").click(function(){
 
-        // show Modal
-        $('#RatingModalDetails').modal('show');
-   });
+     btn.onclick = function () {
+          modal.style.display = "block";
+        }
+ 
+        span.onclick = function () {
+          modal.style.display = "none";
+        }
+ 
+        window.onclick = function (event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
 });
 
 
 //binding array details to the details page
 var index = $('.getDetailsIndex').attr('id');
 
-indexInt= parseInt(index,10);
+indexInt = parseInt(index, 10);
 
 var cardItemslist = JSON.parse(localStorage.getItem("cardsDetails_Local"));
 
-$('.product_details_Img').attr('src',cardItemslist[indexInt].imagesource);
+$('.product_details_Img').attr('src', cardItemslist[indexInt].imagesource);
 $('#product_title').html(cardItemslist[indexInt].itemName);
-$('#price_details').html('$'+cardItemslist[indexInt].price);
+$('#price_details').html('$' + cardItemslist[indexInt].price);
 $('#owner_detail').html(cardItemslist[indexInt].owner);
 $('#description_details').html(cardItemslist[indexInt].description);
-
